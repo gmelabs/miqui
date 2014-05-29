6a37f222873155c864ac97518b742cdfa30678b2
miguel.infante.munoz <miguel.infante.munoz@accenture.com>
2014-05-29 07:35:58 +0200

    Cron with puppet apply script

---
+++ miqui/modules/every_node/files/root/puppet-apply.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export LOGFILE=/root/puppet.log
+export SYSCONF_PATH=/root
+export SYSCONF_DIR=puppet-config
+export SYSCONF_REPO=https://github.com/gmelabs/miqui.git
+export SYSCONF_NAME=miqui
+
+rm -fr ${SYSCONF_PATH}/${SYSCONF_DIR} >> $LOGFILE 2>&1
+cd ${SYSCONF_PATH} >> $LOGFILE 2>&1
+git clone ${SYSCONF_REPO} ${SYSCONF_DIR} >> $LOGFILE 2>&1
+cd ${SYSCONF_PATH}/${SYSCONF_DIR}/${SYSCONF_NAME} >> $LOGFILE 2>&1
+puppet apply --modulepath modules manifests/site.pp >> $LOGFILE 2>&1
--- miqui/modules/every_node/manifests/init.pp
+++ miqui/modules/every_node/manifests/init.pp
@@ -1,9 +1,21 @@
 class every_node {
+  
   file { 'hosts':
     path   => '/etc/hosts',
     source => 'puppet:///modules/every_node/etc/hosts',
     mode   => '0644',
     owner  => 'root',
     group  => 'root',
+  }
+  
+  file { 'puppet-apply':
+    path   => '/root/puppet-apply',
+    source => 'puppet:///modules/every_node/root/puppet-apply.sh',
+    mode   => '0700',
+  }
+  cron { 'date_cron':
+    command => 'sh /root/puppet-apply.sh',
+    minute => "*/5",
+    user => "root",
   }
 }
