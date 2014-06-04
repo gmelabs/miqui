8b95ade445dae93ed0f0ac52cf4676b1c1ab0efa
miguel.infante.munoz <miguel.infante.munoz@accenture.com>
2014-06-04 10:01:16 +0200

    Virtual appliance

--- miqui/manifests/nodes.pp
+++ miqui/manifests/nodes.pp
@@ -1,6 +1,15 @@
 node 'newnode' {
   include every_node
 }
-node 'worker04' {
+node 'master01.bigdata' {
   include every_node
+  include hadoop
+}
+node 'worker01.bigdata' {
+  include every_node
+  include hadoop
+}
+node 'worker02.bigdata' {
+  include every_node
+  include hadoop
 }
