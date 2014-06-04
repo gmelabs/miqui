ae4185e9fe840a44c36401503306ac7112c49525
miguel.infante.munoz <miguel.infante.munoz@accenture.com>
2014-06-04 09:41:14 +0200

    Deleted services node

--- miqui/modules/every_node/manifests/init.pp
+++ miqui/modules/every_node/manifests/init.pp
@@ -7,7 +7,21 @@ class every_node {
     owner  => 'root',
     group  => 'root',
   }
-  
+  file { '.ssh':
+    path   => '/root/.ssh',
+    ensure => 'directory',
+    mode   => '0700',
+    owner  => 'root',
+    group  => 'root',
+  }
+  file { 'known_hosts':
+    path   => '/root/.ssh/known_hosts',
+    source => 'puppet:///modules/every_node/root/.ssh/known_hosts',
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+    require => File['.ssh'],
+  }
   file { 'puppet-apply':
     path   => '/root/puppet-apply.sh',
     source => 'puppet:///modules/every_node/root/puppet-apply.sh',
@@ -17,5 +31,8 @@ class every_node {
     command => 'sh /root/puppet-apply.sh',
     minute => "*/5",
     user => "root",
+  }
+  package { 'tftp':
+    ensure => installed,
   }
 }
