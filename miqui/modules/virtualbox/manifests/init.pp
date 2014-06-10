c449257b3d21093c9a2a4936dbbc259e8e827c43
Javier Teixeira <j.teixeira.quevedo@accenture.com>
2014-06-10 12:39:22 +0200

    Undo last change

--- miqui/modules/virtualbox/manifests/init.pp
+++ miqui/modules/virtualbox/manifests/init.pp
@@ -12,12 +12,12 @@ class virtualbox {
     ensure  => installed, 
     require => [
     Yumrepo['virtualbox', 'epel'],
-    Package['kernel-devel', 'gcc'],
+    Package[$requiredVBpackages],
     ]
   }
   
-  #$requiredVBpackages = ['kernel-devel', 'gcc']
-  package { ['kernel-devel', 'gcc']:
+  $requiredVBpackages = ['kernel-devel', 'gcc']
+  package { $requiredVBpackages:
     ensure => installed,
   }
 
@@ -25,6 +25,6 @@ class virtualbox {
     ensure  => link,
     path    => '/lib/modules/2.6.32-431.el6.x86_64/build',
     target  => '/usr/src/kernels/2.6.32-431.17.1.el6.x86_64',
-    require => Package['kernel-devel', 'gcc'],     
+    require => Package[$requiredVBpackages],     
   }
 }
