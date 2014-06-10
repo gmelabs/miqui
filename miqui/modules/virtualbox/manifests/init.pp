a5e4d06b8cb0df771e033ad1099f2afb80fd4268
Javier Teixeira <j.teixeira.quevedo@accenture.com>
2014-06-10 12:16:08 +0200

    Modify module virtualbox. Changes on installation.

--- miqui/modules/virtualbox/manifests/init.pp
+++ miqui/modules/virtualbox/manifests/init.pp
@@ -7,9 +7,24 @@ class virtualbox {
     gpgcheck => 1,
     gpgkey => 'http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc',
   }
-  
+
   package { 'VirtualBox-4.3': 
     ensure  => installed, 
-    require => Yumrepo['virtualbox'],
+    require => [
+    Yumrepo['virtualbox', 'epel'],
+    Package[$requiredVBpackages],
+    ]
+  }
+  
+  $requiredVBpackages = ['kernel-devel', 'gcc']
+  package { $requiredVBpackages:
+    ensure => installed,
+  }
+
+  file{ 'link-kernels':
+    ensure  => link,
+    path    => '/lib/modules/2.6.32-431.el6.x86_64/build',
+    target  => '/usr/src/kernels/2.6.32-431.17.1.el6.x86_64',
+    require => Package[$requiredVBpackages],     
   }
 }
