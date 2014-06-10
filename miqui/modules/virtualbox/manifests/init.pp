f1dfef9e69f0c82361162ee2ac847f72a86c78ca
Javier Teixeira <j.teixeira.quevedo@accenture.com>
2014-06-10 16:08:51 +0200

    Changes on virtualbox module. Modify dependencies and link statement.

--- miqui/modules/virtualbox/manifests/init.pp
+++ miqui/modules/virtualbox/manifests/init.pp
@@ -2,30 +2,29 @@ class virtualbox {
   
   $requiredVBpackages = ['kernel-devel', 'gcc']
   
-  yumrepo{ 'virtualbox':
+  package { $requiredVBpackages:
+    ensure => installed,
+  }    
+  file{ 'link-kernels':
+    ensure  => link,
+    path    => '/usr/src/kernels/2.6.32-431.el6.x86_64',
+    target  => '/usr/src/kernels/2.6.32-431.17.1.el6.x86_64',
+    require => Package[$requiredVBpackages],
+  }
+  
+  yumrepo { 'virtualbox':
     baseurl  => 'http://download.virtualbox.org/virtualbox/rpm/el/$releasever/$basearch',
     descr    => 'VirtualBox repository',
     enabled  => 1,
     gpgcheck => 1,
-    gpgkey => 'http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc',
+    gpgkey   => 'http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc',
   }
 
   package { 'VirtualBox-4.3': 
     ensure  => installed, 
     require => [
-    Yumrepo['virtualbox'],
-    Package[$requiredVBpackages],
-    ]
-  }  
-  
-  package { $requiredVBpackages:
-    ensure => installed,
-  }
-
-  file{ 'link-kernels':
-    ensure  => link,
-    path    => '/lib/modules/2.6.32-431.el6.x86_64/build',
-    target  => '/usr/src/kernels/2.6.32-431.17.1.el6.x86_64',
-    require => Package[$requiredVBpackages],     
+	    Yumrepo['virtualbox'],
+	    File['link-kernels'],
+	  ]
   }
 }
