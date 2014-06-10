607273e6de74a92c26e3424bd95441c6f0616ee5
miguel.infante.munoz <miguel.infante.munoz@accenture.com>
2014-06-10 21:29:58 +0200

    merge

--- miqui/modules/virtualbox/manifests/init.pp
+++ miqui/modules/virtualbox/manifests/init.pp
@@ -4,8 +4,8 @@ class virtualbox {
   
   package { $requiredVBpackages:
     ensure => installed,
-  }    
-
+  }
+  
   yumrepo { 'virtualbox':
     baseurl  => 'http://download.virtualbox.org/virtualbox/rpm/el/$releasever/$basearch',
     descr    => 'VirtualBox repository',
@@ -13,9 +13,9 @@ class virtualbox {
     gpgcheck => 1,
     gpgkey   => 'http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc',
   }
-
+  
   package { 'VirtualBox-4.3': 
-    ensure  => installed, 
+    ensure  => installed,
     require => [
 	    Yumrepo['virtualbox'],
 	    Package[$requiredVBpackages],
