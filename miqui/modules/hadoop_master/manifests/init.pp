cbfb8180faece88427f0bf3add3433c61188a55c
miguel.infante.munoz <miguel.infante.munoz@accenture.com>
2014-06-15 11:45:58 +0200

    Hadoop tmp master privileges must be set to 755 for hadoop group users to execute mapreduce jobs

--- miqui/modules/hadoop_master/manifests/init.pp
+++ miqui/modules/hadoop_master/manifests/init.pp
@@ -11,17 +11,19 @@ class hadoop_master {
     require => User['hdadmin'],
   }
   
- #En VMs el master también es esclavo y el link no puede crearse  
+  # In virtual appliance, master node is also slave and so link cannot be created. It's ok.
   file { '/data01':
     ensure  => link,
     target  => '/',
     require => User['hdadmin'],
   }
-  # Ya no hace falta. Se ha generado manualmente las claves de hdadmin y distribuida la pública a través de todos los nodos hadoop
-#  exec { 'do-install-rsa-key':
-#    command => '/bin/cp /root/.ssh/id_rsa* /home/hdadmin/.ssh/. && /bin/chown hdadmin:hadoop /home/hdadmin/.ssh/id_rsa*',
-#    creates => '/home/hdadmin/.ssh/id_rsa',
-#    require => File['.ssh', 'hdadmin_ssh_dir'],
-#  }
-
+  
+  file { 'hadoop_master_tmp':
+    path    => '/data01/hadoop/tmp',
+    ensure  => directory,
+    mode    => '0775',
+    owner   => 'hdadmin',
+    group   => 'hadoop',
+    require => File['/data01'],
+  }
 }
