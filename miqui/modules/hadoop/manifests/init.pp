1841db8e6fcebd5f79fbcb64e14fc55dc2dda4d2
Javier Teixeira <j.teixeira.quevedo@accenture.com>
2014-06-25 15:35:29 +0200

    Undo last commit and try to fix the error. Add "remounts => true"

--- miqui/modules/hadoop/manifests/init.pp
+++ miqui/modules/hadoop/manifests/init.pp
@@ -348,7 +348,7 @@ class hadoop::master::mount_data01_hadoop_nn_mirror inherits nfs::mount {
     device   => "${sharedDevice}:${sharedPath}",
     fstype   => 'nfs4',
     ensure   => 'mounted',
-    remounts => false,
+    remounts => true,
     options  => 'defaults',
     atboot   => true,
     require  => File[$requiredResourceId],
