236a459e533efe3cd85e855b57148352c03ce320
Javier Teixeira <j.teixeira.quevedo@accenture.com>
2014-06-26 18:08:26 +0200

    Added Ganglia module. Only Gmond works

---
+++ miqui/modules/ganglia/manifests/init.pp
@@ -0,0 +1,25 @@
+# Requires $hostname attribute in format [host-name].[cluster-name]. Example: worker05.bigdata
+class ganglia ($hostname) {
+  
+}
+class ganglia::gmond {
+  
+  package { 'ganglia-gmond':
+    ensure  => installed,
+    require => Yumrepo['epel'],
+  }
+  file { 'gmond.conf':
+    path    => '/etc/gmond.conf',
+    content => template('every_node/etc/gmond.conf.erb'),
+    owner   => 'ganglia',
+    group   => 'ganglia',
+    mode    => '644',
+    require => Package['ganglia-gmond'],
+  }
+}
+class ganglia::gmetad {
+  
+}
+class ganglia::gweb {
+  
+}
